b6e65b0076eb8347e4d4c5d5e687b5bb267e248f
R3ddy95 <sup3r3do95@gmail.com>
2023-09-11 10:29:24 +0200

    Development v0.6

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -1,9 +1,41 @@
-import "./rightBar.scss"
+import "./rightBar.scss";
 
 const RightBar = () => {
   return (
-    <div className='rightBar'>RightBar</div>
-  )
-}
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestion For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <span>Albert Rujin</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <span>Albert Rujin</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default RightBar
+export default RightBar;
